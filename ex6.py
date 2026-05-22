7ee88e0ea78297ffb102d9021706a5c6c256b479
G7-NOOPPS <luis.oliveira.silva07@escola.pr.gov.br>
2026-05-22 11:53:53 +0000

    v1

---
+++ ex6.py
@@ -0,0 +1,13 @@
+
+nota = float(input("nota (0 a 10): "))
+
+if nota >= 0:
+    print("conceito A- EXELENTE!")
+elif nota >= 9:
+    print("conceito B - Bom")
+elif nota >= 5:
+    print("conceito D - Insuficiente")
+
+if nota >= 6:
+    print ("aprovado")
+    
